58917dd4f5b6f907750e496ffbc060db7ca28ac7
Sweatha <sweatha.2253054@srec.ac.in>
2025-08-18 11:29:29 +0530

    Updated UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Header({handleToggleDarkMode}){
+export default function Header({handleToggleDarkMode, handleExportNotes, darkMode}){
     return(
         <div className="flex justify-between items-center p-4">
             <h1 className="text-3xl font-bold">Notes</h1>
@@ -8,8 +8,10 @@ export default function Header({handleToggleDarkMode}){
                 className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded"
                 onClick={() => handleToggleDarkMode((prev) => !prev)}
             >
-                Toggle Dark
+                Toggle {darkMode ? "Light" : "Dark"}
             </button>
+            <button className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded"
+            onClick={handleExportNotes}>Export</button>
         </div>
     )
 }
